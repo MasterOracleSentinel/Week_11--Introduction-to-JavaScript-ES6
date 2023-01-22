43a6852eb62d65c4896a018d7033c1ad45e90900
K. Artis-Mickens <105542222+MasterOracleSentinel@users.noreply.github.com>
2023-01-21 22:54:57 -0500

    Create rest.js

---
+++ Spread And Rest Operators/rest.js
@@ -0,0 +1,20 @@
+// TODO: create the function sumAll
+function sumAll(x, y, ...rest)
+{
+  let total = 0;
+  for (const arg of rest)
+  {
+    total += arg;
+  }
+  return (x + y + total);
+}
+
+
+console.log(sumAll(1, 2)); // expected output: 3
+console.log(sumAll(1, 2, 3)); // expected output: 6
+console.log(sumAll(1, 2, 3, 4)); // expected output: 10
+
+//don't change this line
+if (typeof module !== "undefined") {
+  module.exports = { sumAll };
+}
